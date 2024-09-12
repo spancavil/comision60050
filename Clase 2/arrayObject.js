b66e98837d965a6eafafb614f47a5cea2ac59db3
Sebas <soancavil@gmail.com>
2024-09-11 22:47:32 -0300

    Desarrollo clase 2

--- Clase 2/arrayObject.js
+++ Clase 2/arrayObject.js
@@ -1,13 +1,24 @@
 //Array, array positions, methods and properties
+const nombres = ["Jorge", "Walter", "Lucas", "Maira"]
+
+console.log(nombres[1]);
 
 //Array last position
+console.log(nombres[nombres.length - 1])
 
 //Array destructuring
+const [nombre1, nombre2, ...restoDeNombres] = nombres
+
+console.log(nombre1);
+console.log(restoDeNombres);
 
 //Copying arrays
+
 //Copy by reference => NO X
+// const nombres2 = nombres
 
 //Copia by value => OK
+const nombres2 = [...nombres]
 
 //find - Returns the reference
 const inventory = [
@@ -17,16 +28,28 @@ const inventory = [
 ];
 
 function isCherries(fruit) {
-  return fruit.name === "cherries";
+  return fruit.name === "apples";
 }
 //Find pasa la referencia al valor, no el valor en sí. Por lo tanto si lo cambio, también cambiará en inventory.
-const value = inventory.find(isCherries);
+const value = inventory.find(
+  fruit => fruit.name === "apples"
+);
 value.name = "kiwi";
 console.log(inventory);
 
 //Map - Returns new array
+const fruits2 = inventory.map(fruit => {
+  return {
+    name: fruit.name,
+    quantity: fruit.quantity + 1
+  }
+})
+
+console.log(fruits2);
 
 //filter
+const fruitsFiltered = inventory.filter(fruit => fruit.quantity > 0)
+console.log(fruitsFiltered);
 
 //Ejercicios
 //1. Encontrar mediante el método find un nombre similar a "edu" en el array ["Jose", "Roberto", "Josefina", "Eduardo", "Bedufino", "Simona"]
