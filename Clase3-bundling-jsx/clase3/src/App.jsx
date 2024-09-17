e63ea7590b449679b7d2b7f6f16642b07f91020b
Sebas <soancavil@gmail.com>
2024-09-16 23:20:58 -0300

    Desarrollo clase 3

--- Clase3-bundling-jsx/clase3/src/App.jsx
+++ Clase3-bundling-jsx/clase3/src/App.jsx
@@ -8,35 +8,35 @@ import NavBar from './components/NavBar'
 function App() {
 
   // 1. Styling: inline styles directly and from separated variable
+  const buttonStyles = {
+    padding: 10,
+    border: '1px solid white',
+    borderRadius: 40,
+    backgroundColor: 'green'
+  }
+
   // 2. Image (autoclosing tag) 
   // 3. Classname
+
   // 4. onClick introduction (camelCase)
+  const handleClick = () => {
+    console.log("Se hizo click en el botón");    
+  }
 
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
+  //JSX => me define cómo se verá la interfaz.
+
+  //<> </> => React Fragment, no añade ninguna etiqueta al HTML
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 style={{color: 'blueviolet', marginTop: 40}} >Hola, bienvenidos a la clase 3!</h1>
+      <button style={buttonStyles} onClick={handleClick}>Press me</button>
+      <img src={kitten} style={{width: 400}}/>
+      <hr/>
+      <div className='container' >
+        <span>Hola dentro del div</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <NavBar/>
     </>
   )
 }
--- Clase3-bundling-jsx/sugarSyntax.js
+++ Clase3-bundling-jsx/sugarSyntax.js
@@ -10,7 +10,7 @@ class Circle {
 
     //TODO
     get perimeter() {
-
+        return this.calcPerimeter()
     }
 
     calcArea() {
@@ -18,22 +18,39 @@ class Circle {
     }
 
     //TODO
-    getPerimeter() {
-
+    calcPerimeter() {
+        return Math.PI * 2 * this.radius
     }
 }
 
 //object instances
+/* const circle1 = new Circle(1)
+console.log(circle1.area)
+console.log(circle1.perimeter); */
+
+/* const condicion = true && false // false
+if (condicion) {
+    //pasa algo
+    console.log("Entra al if")
+} else {
+    //pasa otra cosa
+    console.log("Entra al else")
+} */
 
 //OPERADOR TERNARIO
 
 //Función que retorna en base a la opción elegida mate dulce o mate amargo
-//Outpus: 
+//Outputs: 
 //menos de 1 cucharada de azúcar => amargo
 //más de 1 cucharadas azúcar => Dulce
 const dulceOAmargo = (cucharadas) => {
     //add logic
+    return cucharadas < 1 ? 'amargo' : cucharadas <= 2 ? 'semiamargo' : 'dulce'
 }
+
+const mateDulce = dulceOAmargo(3)
+
+console.log(mateDulce);
 
 //Destructuring y spread operator ya vistos en clase 1 y 2
 
