284575bd95d3138c785fa63d72181315c31b0812
Sebas <soancavil@gmail.com>
2024-09-18 11:16:02 -0300

    Added template clase 4

--- Clase4-componentes-I/clase4/src/App.jsx
+++ Clase4-componentes-I/clase4/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <img src={cart} className="logo" alt="cart"/>
       </div>
       <h1>Vite + React</h1>
       <div className="card" onMouseEnter={()=>{}}>
