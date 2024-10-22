898649934c95ec439c055fe4432258ddc1d9829b
Sebas <soancavil@gmail.com>
2024-10-21 23:37:45 -0300

    Desarrollo clase 13

--- Clase13-firebaseI/clase13/src/components/ItemListContainer.jsx
+++ Clase13-firebaseI/clase13/src/components/ItemListContainer.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react"
 import ItemList from "./ItemList"
 import { useParams } from "react-router-dom"
 import mockProducts from "../assets/mockData.json"
+import { db } from "../firebase/config"
+import { collection, query, where, getDocs } from "firebase/firestore"
 
 //Logica para traer los productos. Promise, setTimeout etc.
 const ItemListContainer = () => {
@@ -11,15 +13,44 @@ const ItemListContainer = () => {
     const { categoryId } = useParams()
 
     useEffect(() => {
-        let productsFiltered
-        if (categoryId) {
+        ;(async () => {
+            try {
+                let productsFiltered = []
+
+                if (categoryId) {
+                    const q = query(
+                        collection(db, "products"),
+                        where("category", "==", categoryId)
+                    )
+
+                    const querySnapshot = await getDocs(q)
+                    querySnapshot.forEach((doc) => {
+                        // doc.data() is never undefined for query doc snapshots
+                        console.log(doc.id, " => ", doc.data())
+                        productsFiltered.push({ id: doc.id, ...doc.data() })
+                    })
+                } else {
+                    const querySnapshot = await getDocs(
+                        collection(db, "products")
+                    )
+                    querySnapshot.forEach((doc) => {
+                        console.log(`${doc.id} => ${doc.data()}`)
+                        productsFiltered.push({ id: doc.id, ...doc.data() })
+                    })
+                }
+                setProducts(productsFiltered)
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+        /* if (categoryId) {
             productsFiltered = mockProducts.filter(
                 (product) => product.category === categoryId
             )
         } else {
             productsFiltered = mockProducts
-        }
-        setProducts(productsFiltered)
+        } */
+        // setProducts(productsFiltered)
     }, [categoryId])
 
     /* useEffect(()=> {
--- Clase13-firebaseI/clase13/src/firebase/config.js
+++ Clase13-firebaseI/clase13/src/firebase/config.js
@@ -7,7 +7,13 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-}
+    apiKey: "AIzaSyBHCXExyUaUEImDjOgFp7DV7cAKgvGgGQA",
+    authDomain: "ecommerce-60050-c225d.firebaseapp.com",
+    projectId: "ecommerce-60050-c225d",
+    storageBucket: "ecommerce-60050-c225d.appspot.com",
+    messagingSenderId: "178963455221",
+    appId: "1:178963455221:web:7c1fe6a7b2c4914f82b9bc"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
